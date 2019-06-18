c8523e995282d622b44a8dcb917b4d610d3269bc
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2019-06-18 00:40:04 -0400

    written part and comments

--- Assignment/A2/multiagent/multiAgents.py
+++ Assignment/A2/multiagent/multiAgents.py
@@ -74,15 +74,23 @@ class ReflexAgent(Agent):
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
         "*** YOUR CODE HERE ***"
+        # base value of evaluation
         heur_val = scoreEvaluationFunction(successorGameState) + 1.0 * (max(newScaredTimes) + min(newScaredTimes))
         newGhostPosition = successorGameState.getGhostPositions()
         newFoodPosition = newFood.asList()
 
+        # evaluation returns infinity if win, or negative infinity if lose
         if successorGameState.isWin():
           return float('inf')
         elif successorGameState.isLose():
           return -float('inf')
 
+        # check ghost in the game, if the ghost is pretty close, decrease
+        # the evaluation value by 1.0/manhattan distance between position of
+        # pacman and the ghost in the next game state. If they are in the same
+        # spot, then the ghost is in scare time, since the if the ghost is not
+        # in scare time, then the pacman loses. In this case, the evaluation value
+        # increases by 1.0.
         for ghostPos in newGhostPosition:
           ghostDistance = manhattanDistance(newPos,ghostPos)
           if ghostDistance <= 3:
@@ -91,6 +99,10 @@ class ReflexAgent(Agent):
             else:
               heur_val -= 1.0 / ghostDistance
         
+        # check if there is any food nearby. If any food is around, but not eaten
+        # by the pacman, the evaluation value increases by 1.0/manhattan distance between
+        # the food and the pacman. If the pacman can eat the food, the evaluation value
+        # increases by 1.0
         for foodPos in newFoodPosition:
           foodDistance = manhattanDistance(newPos, foodPos)
           if foodDistance == 0:
@@ -165,28 +177,39 @@ class MinimaxAgent(MultiAgentSearchAgent):
         return bestMove
     
     def getActionHelper(self, depth, agentIndex, gameState):
+      """
+        recursively find the score gained and the best action should be taken at
+        the state input, used by minimax agent. The max player (pacman) tends to
+        maximize the score while the min player (ghost) tends to minimize the score
+      """
       action = None
+      # if the player win or lose or reach the depth of the game search tree,
+      # return the score at the current state and no action can be taken
       if depth == self.depth or gameState.isWin() or gameState.isLose():
         return (self.evaluationFunction(gameState), action)
       
-      if agentIndex == 0:
+      if agentIndex == 0: # max player
         score = -float('inf')
-      else:
+      else: # min player
         score = float('inf')
   
+      # get all actions can be taken at current agent
       actions = gameState.getLegalActions(agentIndex)
       
+      # get the successor from all actions
+      # and evaluate the score for the best action
+      # for max and min player respecively
       for act in actions:
         nextState = gameState.generateSuccessor(agentIndex, act)
-        if isLastAgent(gameState, agentIndex):
+        if isLastAgent(gameState, agentIndex): # next turn is for max player
           nextScore, nextAction = self.getActionHelper(depth + 1, 0, nextState)
-        else:
+        else: # next turn is for min player
           nextScore, nextAction = self.getActionHelper(depth, agentIndex + 1, nextState)
 
-        if agentIndex == 0 and nextScore > score:
+        if agentIndex == 0 and nextScore > score: # max player and the score of child node > score currently
           score = nextScore
           action = act
-        elif agentIndex != 0 and nextScore < score:
+        elif agentIndex != 0 and nextScore < score: # min player and the score of child node < score currently
           score = nextScore
           action = act
       
@@ -207,40 +230,61 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         return bestMove
     
     def getActionHelper(self, depth, agentIndex, gameState, alpha, beta):
+      """
+        recursively find the score gained and the best action should be taken at
+        the state input, used by alpha-beta agent. The max player (pacman) tends to
+        maximize the score while the min player (ghost) tends to minimize the score
+
+        Cutting min node n (for max player):
+        alpha - max of n's sibling
+        beta - min of n's children
+        
+        Cutting max node n (for min player):
+        alpha - max of n's children
+        beta - min of n's sibling
+      """
       action = None
+      # if the player win or lose or reach the depth of the game search tree,
+      # return the score at the current state and no action can be taken
       if depth == self.depth or gameState.isWin() or gameState.isLose():
         return (self.evaluationFunction(gameState), action)
   
-      actions = gameState.getLegalActions(agentIndex)
-      if agentIndex == 0:
+      if agentIndex == 0: # max player
         score = -float('inf')
-      else:
+      else: # min player
         score = float('inf')
       
+      # get all actions can be made at the current agent
+      actions = gameState.getLegalActions(agentIndex)
+      
+      # get the successor from all actions
+      # and evaluate the score for the best action
+      # for max and min player respecively
+      # use pruning (break) to save time
       for act in actions:
         nextState = gameState.generateSuccessor(agentIndex, act)
-        if isLastAgent(gameState, agentIndex):
+        if isLastAgent(gameState, agentIndex): # next turn is for max player
           nextScore, nextAction = self.getActionHelper(depth + 1, 0, nextState, alpha, beta)
-        else:
+        else: # next turn is for min player
           nextScore, nextAction = self.getActionHelper(depth, agentIndex + 1, nextState, alpha, beta)
 
-        if agentIndex == 0:
-          alpha = max(alpha, nextScore)
-          if nextScore > score:
+        if agentIndex == 0: # next turn is for max player
+          alpha = max(alpha, nextScore) # alpha always takes max value between itself and the score of the child
+          if nextScore > score: # for max player, always try to increase score
             score = nextScore
             action = act
-        else:
-          beta = min(beta, nextScore)
-          if score < nextScore:
+        else: # next turn is for min turn
+          beta = min(beta, nextScore) # beta always takes min value between itself and the score of the child
+          if score < nextScore: # for min player, always try to decrease score
             score = nextScore
             action = act
 
-        if beta <= alpha:
+        if beta <= alpha: # break if alpha exceeds beta
             break
       
-      if agentIndex == 0:
+      if agentIndex == 0: # max player, return alpha for maximization
         return (alpha, action)
-      else:
+      else: # min player, return beta for minimization
         return (beta, action)
 
 
@@ -261,38 +305,59 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         return bestMove
     
     def getActionHelper(self, depth, agentIndex, gameState):
+      """
+        recursively find the score gained and the best action should be taken at
+        the state input, used by minimax agent. The max player (pacman) tends to
+        maximize the score while the chance player (ghost) tends to pick the score
+        by probability
+      """
       action = None
+      # if the player win or lose or reach the depth of the game search tree,
+      # return the score at the current state and no action can be taken
       if depth == self.depth or gameState.isWin() or gameState.isLose():
         return (self.evaluationFunction(gameState), action)
-      
-      actions = gameState.getLegalActions(agentIndex)
-      if agentIndex == 0:
+  
+      if agentIndex == 0: # max player
         score = -float('inf')
-      else:
+      else: # min player
         score = float('inf')
       
+      # get all actions can be made at the current agent
+      actions = gameState.getLegalActions(agentIndex)
+      
+      # set the sum of scores and the number of scores for chance player
+      # in order to find the average socre for chance player
       chanceScore = 0
       chanceScoreTimes = 0
 
+      # get the successor from all actions
+      # and evaluate the score for the best action
+      # for max and mchance player respecively
       for act in actions:
+        # find the next state caused by the act
         nextState = gameState.generateSuccessor(agentIndex, act)
-        if isLastAgent(gameState, agentIndex):
+        if isLastAgent(gameState, agentIndex): # next turn is for max player
           nextScore, nextAction = self.getActionHelper(depth + 1, 0, nextState)
-        else:
+        else: # next turn is for chance player
           nextScore, nextAction = self.getActionHelper(depth, agentIndex + 1, nextState)
         
-        if agentIndex == 0:
+        if agentIndex == 0: # get best action and score for max player
           if score < nextScore:
             score = nextScore
             action = act
-        elif agentIndex != 0:
+        elif agentIndex != 0: # get score for chance player, action does not change
           chanceScore += nextScore
           chanceScoreTimes += 1
+          # call probability method to get the score of the chance player
           score = self.__prob(chanceScore, chanceScoreTimes)
 
       return (score, action)
     
     def __prob(self, chanceScore, chanceScoreTimes):
+      """
+        compute the score for the chance score
+        i.e., find the average of scores of chance player
+      """
       return float(chanceScore) / float(chanceScoreTimes)
 
 
@@ -304,6 +369,7 @@ def betterEvaluationFunction(currentGameState):
       DESCRIPTION: <write something here so we know what you did>
     """
     "*** YOUR CODE HERE ***"
+    # get all useful information
     pacmanPosition = currentGameState.getPacmanPosition()
     ghostPositions = currentGameState.getGhostPositions()
     foodPositions = currentGameState.getFood().asList()
@@ -311,28 +377,36 @@ def betterEvaluationFunction(currentGameState):
     ghostScaredTimes = [ghostState.scaredTimer for ghostState in ghostStates]
 
 
+    # get the basic value for evaluation
     heur_val = scoreEvaluationFunction(currentGameState) + 2.75 * (max(ghostScaredTimes) + min(ghostScaredTimes))
 
+    # evaluation returns infinity if win, or negative infinity if lose
     if currentGameState.isWin():
       return float('inf')
     elif currentGameState.isLose():
       return -float('inf')
 
-    ghostDistances = []
-    foodDistances = []
     
+    # check ghost in the game, if the ghost is pretty close, decrease
+    # the evaluation value by 2.5/manhattan distance between position of
+    # pacman and the ghost in the next game state. If they are in the same
+    # spot, then the ghost is in scare time, since the if the ghost is not
+    # in scare time, then the pacman loses. In this case, the evaluation value
+    # increases by 2.0. (For better evaluation, there are weights for factors)
     for ghostPos in ghostPositions:
       ghostDistance = manhattanDistance(pacmanPosition,ghostPos)
-      ghostDistances.append(ghostDistance)
       if ghostDistance <= 3:
         if ghostDistance == 0:
           heur_val += 2,0
         else:
           heur_val -= 2.5 / ghostDistance
     
+     # check if there is any food nearby. If any food is around, but not eaten
+    # by the pacman, the evaluation value increases by 2.0/manhattan distance between
+    # the food and the pacman. If the pacman can eat the food, the evaluation value
+    # increases by 2.0. (For better evaluation, there are weights for factors)
     for foodPos in foodPositions:
       foodDistance = manhattanDistance(pacmanPosition, foodPos)
-      foodDistances.append(foodDistance)
       if foodDistance == 0:
         heur_val += 2.0
       elif foodDistance > 0:
