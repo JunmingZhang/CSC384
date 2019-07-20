1b3859bbd0ca2a2accb5269e5cacd38d06ec1c73
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2019-07-20 05:19:19 -0400

    complete two models

--- Assignment/A3/csp_code/kenken_csp.py
+++ Assignment/A3/csp_code/kenken_csp.py
@@ -34,10 +34,71 @@ import itertools
 
 def binary_ne_grid(kenken_grid):
     ##IMPLEMENT
-    
+	model = CSP("binary_ne_grid")
+	grid = []
+	grid_size = kenken_grid[0][0]
+	domain = list(range(1, grid_size + 1))
+	binary_ne_tups = []
+
+	for row in range(grid_size):
+		grid.append([])
+		for col in range(grid_size):
+			var = Variable(str(row + 1) + str(col + 1), domain)
+			model.add_var(var)
+			grid[row].append(var)
+
+			if row != col:
+				binary_ne_tups.append((row + 1, col + 1))
+
+	for row in range(grid_size):
+		for col in range(grid_size):
+			for row_gap in range(row + 1, grid_size):
+				name = str(row) + str(col) + ' vs ' + str(row_gap) + str(col)
+				constraint = Constraint(name, [grid[row][col], grid[row_gap][col]])
+				constraint.add_satisfying_tuples(binary_ne_tups)
+				model.add_constraint(constraint)
+
+			for col_gap in range(col + 1, grid_size):
+				name = str(row) + str(col) + ' vs ' + str(row) + str(col_gap)
+				constraint = Constraint(name, [grid[row][col], grid[row][col_gap]])
+				constraint.add_satisfying_tuples(binary_ne_tups)
+				model.add_constraint(constraint)
+
+	return model, grid
 
 def nary_ad_grid(kenken_grid):
-    ##IMPLEMENT 
+    ##IMPLEMENT
+	model = CSP("nary_ad_grid")
+	grid = []
+	grid_size = kenken_grid[0][0]
+	domain = list(range(1, grid_size + 1))
+
+	nary_ad_tups = list(itertools.permutations(domain))
+
+	for row in range(grid_size):
+		grid.append([])
+		for col in range(grid_size):
+			var = Variable(str(row + 1) + str(col + 1), domain)
+			model.add_var(var)
+			grid[row].append(var)
+	
+	for ind in range(grid_size):
+		name = "row " + str(ind)
+		row_scope = grid[ind]
+
+		constraint = Constraint(name, row_scope)
+		constraint.add_satisfying_tuples(nary_ad_grid)
+		model.add_constraint(constraint)
+
+		name = "col " + str(ind)
+		col_scope = [row[ind] for row in grid]
+
+		constraint = Constraint(name, col_scope)
+		constraint.add_satisfying_tuples(nary_ad_grid)
+		model.add_constraint(constraint)
+	
+	return model, grid
+
     
 
 def kenken_csp_model(kenken_grid):
